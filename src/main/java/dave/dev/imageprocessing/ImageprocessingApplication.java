ecf4996bb68b66c8b94359c479c14d06a011aecb
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-27 19:57:59 +0800

    image processing

---
+++ src/main/java/dave/dev/imageprocessing/ImageprocessingApplication.java
@@ -0,0 +1,13 @@
+package dave.dev.imageprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dave/dev/imageprocessing/ImageprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.imageprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
