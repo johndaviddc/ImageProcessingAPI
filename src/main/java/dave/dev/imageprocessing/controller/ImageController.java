3fdb7fc8285ad88cc6548cf41162df1f71b72b15
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-31 10:25:11 +0800

    added controller

---
+++ src/main/java/dave/dev/imageprocessing/controller/ImageController.java
@@ -0,0 +1,21 @@
+package dave.dev.imageprocessing.controller;
+
+import dave.dev.imageprocessing.model.Image;
+import dave.dev.imageprocessing.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    
+}
